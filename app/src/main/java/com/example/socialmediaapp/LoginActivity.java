e5e63f4a87dcc99661ca31cb067eadbe1d8efce9
Alexander Hom <alexanderhom19@gmail.com>
2022-04-25 16:27:03 -0700

    more stylistic updates to login, register activity

--- app/src/main/java/com/example/socialmediaapp/LoginActivity.java
+++ app/src/main/java/com/example/socialmediaapp/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.socialmediaapp;
 
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,16 +17,22 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUserIdentifier;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // hide the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         // get widgets from the layout
         etUserIdentifier = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
         // add a click listener to the login button
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -36,9 +45,18 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(userIdentifier, password);
             }
         });
+
+        // add a click listener to the sign up button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to the Register activity
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 
     private void loginUser(String userIdentifier, String password) {
-        // validate user
+        // TODO: validate user
     }
 }
--- app/src/main/java/com/example/socialmediaapp/RegisterActivity.java
+++ app/src/main/java/com/example/socialmediaapp/RegisterActivity.java
@@ -1,11 +1,15 @@
 package com.example.socialmediaapp;
 
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,12 +21,17 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText etPassword;
     private EditText etConfirmPassword;
     private Button btnRegister;
+    private Button btnSignIn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // hide the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         // get the widgets from the layout
         etUsername = findViewById(R.id.etUsername);
@@ -31,9 +40,10 @@ public class RegisterActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         etConfirmPassword = findViewById(R.id.etConfirmPassword);
         btnRegister = findViewById(R.id.btnRegister);
+        btnSignIn = findViewById(R.id.btnSignIn);
 
+        // add a click listener to the register button
         btnRegister.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 String username = etUsername.getText().toString();
@@ -44,11 +54,23 @@ public class RegisterActivity extends AppCompatActivity {
 
                 // check if the password matches
                 if (!password.equals(confirmPassword)) {
-                    // TODO: handle when passwords are not equal
+                    // display error message
+                    Toast.makeText(getApplicationContext(),
+                            getString(R.string.passwords_dont_match),
+                            Toast.LENGTH_SHORT).show();
                 }
 
                 User user = new User(username, email, dateOfBirth, password);
                 registerUser(user);
+            }
+        });
+
+        // add a click listener to the sign in button
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to Login activity
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             }
         });
     }
