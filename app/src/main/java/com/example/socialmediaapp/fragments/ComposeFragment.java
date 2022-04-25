c66a59e74962ad8e3e38ed29354f66850dd3ec98
Alexander Hom <alexanderhom19@gmail.com>
2022-04-25 10:52:06 -0700

    add register activity layout

--- app/src/main/java/com/example/socialmediaapp/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/socialmediaapp/fragments/ComposeFragment.java
@@ -65,7 +65,7 @@ public class ComposeFragment extends Fragment {
         etDescription = view.findViewById(R.id.etDescription);
         btnCaptureImage = view.findViewById(R.id.btnCaptureImage);
         ivPostImage = view.findViewById(R.id.ivPostImage);
-        btnSubmit = view.findViewById(R.id.btnSubmit);
+        btnSubmit = view.findViewById(R.id.btnLogin);
 
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
