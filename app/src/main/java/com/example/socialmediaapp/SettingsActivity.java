0cadeab463ee322ed3313485c8b205c06e555100
Alexander Hom <alexanderhom19@gmail.com>
2022-04-30 12:17:32 -0700

    add logout functionality

--- app/src/main/java/com/example/socialmediaapp/SettingsActivity.java
+++ app/src/main/java/com/example/socialmediaapp/SettingsActivity.java
@@ -6,13 +6,25 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class SettingsActivity extends AppCompatActivity {
 
     public static final String TAG = "SettingsActivity";
     private Toolbar toolbarSecondary;
     private TextView tvToolbarTitle;
+
+    private Button btnUpdateUsername;
+    private Button btnUpdateEmail;
+    private Button btnUpdatePassword;
+    private Button btnUpdateBio;
+    private Button btnLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,12 @@ public class SettingsActivity extends AppCompatActivity {
         tvToolbarTitle = findViewById(R.id.tvToolbarTitle);
         tvToolbarTitle.setText(R.string.settings);
 
+        btnUpdateUsername = findViewById(R.id.btnUpdateUsername);
+        btnUpdateEmail = findViewById(R.id.btnUpdateEmail);
+        btnUpdatePassword = findViewById(R.id.btnUpdatePassword);
+        btnUpdateBio = findViewById(R.id.btnUpdateBio);
+        btnLogOut = findViewById(R.id.btnLogOut);
+
         // handle back button click
         toolbarSecondary.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +49,24 @@ public class SettingsActivity extends AppCompatActivity {
                 // go back to the main activity
                 Intent i = new Intent(SettingsActivity.this, MainActivity.class);
                 startActivity(i);
+            }
+        });
+
+        // log out the user
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(SettingsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        Intent i = new Intent(SettingsActivity.this, LoginActivity.class);
+                        startActivity(i);
+                    }
+                });
             }
         });
     }
